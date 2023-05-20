9b94e91cdfd8ef1a554ff189383e51ef0168ffe5
om <omjbhaleraowork.com>
2023-05-21 00:13:55 +0530

    first commit

---
+++ 2.Print integer in java/Integers.java
@@ -0,0 +1,9 @@
+public class Integers {
+    public static void main(String[] args) {
+        int c; // declaring a variable
+        /* Using for loop to repeat instruction execution */
+        for (c = 1; c <= 10; c++) {
+            System.out.println(c);
+        }
+    }
+}
