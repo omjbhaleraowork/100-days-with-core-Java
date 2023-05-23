1e525f40c99412bf8220be820aeef8e54019f2d5
om <omjbhaleraowork.com>
2023-05-23 22:09:57 +0530

    day-8

--- 8.add two numbers/Addnumbers.java
+++ 8.add two numbers/Addnumbers.java
@@ -11,3 +11,22 @@ class Addnumbers {
         System.out.println("Sum of entered integers = " + z);
     }
 }
+// For Large Number
+// import java.util.Scanner;
+// import java.math.BigInteger;
+
+// class AddingLargeNumbers {
+// public static void main(String[] args) {
+// String number1, number2;
+// Scanner in = new Scanner(System.in);
+// System.out.println("Enter first large number");
+// number1 = in.nextLine();
+// System.out.println("Enter second large number");
+// number2 = in.nextLine();
+// BigInteger first = new BigInteger(number1);
+// BigInteger second = new BigInteger(number2);
+// BigInteger sum;
+// sum = first.add(second);
+// System.out.println("Result of addition = " + sum);
+// }
+// }
