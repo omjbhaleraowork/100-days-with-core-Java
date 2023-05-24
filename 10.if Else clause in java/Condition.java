6f623cd12c38e7e9a600e4b5b64d91845d696c2e
om <omjbhaleraowork.com>
2023-05-24 18:51:44 +0530

    day-10

---
+++ 10.if Else clause in java/Condition.java
@@ -0,0 +1,10 @@
+public class Condition {
+    public static void main(String[] args) {
+        boolean learning = false;
+        if (learning) {
+            System.out.println(" Java programmer");
+        } else {
+            System.out.println("What are you doing here?");
+        }
+    }
+}
