8abb58e4315a7e3b0bdb3200c85b6d7926412d43
om <omjbhaleraowork.com>
2023-06-11 21:55:39 +0530

    day-16

---
+++ 16.for loop/ForLoop.java
@@ -0,0 +1,12 @@
+/**
+ * ForLoop
+ */
+public class ForLoop {
+
+    public static void main(String[] args) {
+        int c;
+        for (c = 1; c <= 10; c++) {
+            System.out.println(c);
+        }
+    }
+}
